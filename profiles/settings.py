435cdd1d5f4f2142dec59721894b94dccab32708
Mwapsam <mwapsam@gmail.com>
2022-09-15 13:32:26 +0200

    Setup environmental variables

--- profiles/settings.py
+++ profiles/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4!l=nq6cb35cudgbp0q2vt@ktdhv#shu3ep0^y!x0tr-b1mkp4'
+# SECRET_KEY = 'django-insecure-4!l=nq6cb35cudgbp0q2vt@ktdhv#shu3ep0^y!x0tr-b1mkp4'
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
