1e2383a9b4f5ab85a828be59a30429b5f7600376
Mwapsam <mwapsam@gmail.com>
2022-09-23 09:36:17 +0200

    Remove people object from auth urls

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,14 +1,20 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
-from .views import RegisterView, LoginView, UserView, LogoutView, ActivateAccount
+from rest_framework.urlpatterns import format_suffix_patterns
+from authentication import views
 
 urlpatterns = [
-    path('register', RegisterView.as_view()),
-    path('login', LoginView.as_view()),
-    path('user', UserView.as_view()),
-    path('logout', LogoutView.as_view()),
-    path('activate/<uid>/<token>/', ActivateAccount.as_view(), name='activate'),
+    path('register', views.RegisterView.as_view()),
+    path('login', views.LoginView.as_view()),
+    path('user', views.UserView.as_view()),
+    path('logout', views.LogoutView.as_view()),
+    path('activate/<uid>/<token>/', views.ActivateAccount.as_view(), name='activate'),
     path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', jwt_views.TokenVerifyView.as_view(), name='token_verify'),
+    # path('profiles', views.ProfilesList.as_view()),
+    # path('people/<int:pk>/', views.ProfileDetailView.as_view()),
+    # path('create', views.ProfileCreate.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
